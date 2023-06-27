650fa5f204bb23a6c8d06f24d8f7ea688762ea13
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-06-27 14:52:13 +0600

    1182-Column_in_Array.java

---
+++ 1182-Column_in_Array.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.util.*;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        Scanner timtim = new Scanner(System.in);
+
+        int N = timtim.nextInt();
+        char T = timtim.next().charAt(0);
+        double[][] X = new double[12][12];
+
+        for(int i=0; i<X.length; i++)
+        {
+            for(int j=0; j<X[i].length; j++)
+            {
+                X[i][j] = timtim.nextDouble();
+            }
+        }
+
+        switch (T)
+        {
+            case 'S':
+                double somaa = 0;
+                for(int i=0; i<X[0].length; i++)
+                {
+                    somaa += X[i][N];
+                }
+                System.out.printf("%.1f", somaa);
+                break;
+            case 'M':
+                double media = 0;
+                for(int i=0; i<X[0].length; i++)
+                {
+                    media+= X[i][N];
+                }
+                media /= X[0].length;
+                System.out.printf("%.1f", media);
+                break;
+
+
+        }
+        System.out.println();
+ 
+    }
+ 
+}
